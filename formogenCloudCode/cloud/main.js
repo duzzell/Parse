d794ca9d1407a761489a72bc65b2fcf873018abd
duzzell <david.u@westecapps.com>
2015-02-06 16:05:39 -0600

    First stab at Unique trigger for Recipients

--- formogenCloudCode/cloud/main.js
+++ formogenCloudCode/cloud/main.js
@@ -4,3 +4,39 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello David! Love ya, buddy.");
 });
+
+//Using beforeSave to ensure all recipients are unique by email address
+var RemoteRecipient = Parse.Object.extend("RemoteRecipient");
+
+//check if email has a value
+//enforce uniqueness based on the email column
+Parse.Cloud.beforeSave("RemoteRecipient", function(request, response)
+{
+	if(!request.object.get("email"))
+	{
+		response.error('A RemoteRecipient must have an email address.');
+	}
+	else
+	{
+		var query = new Parse.Query(RemoteRecipient);
+		query.equalTo("email", request.objet.get("email"));
+		query.first
+		({
+			success: function(object)
+			{
+				if (object)
+				{
+					response.error("A RemoteRecipient with this email already exists.");
+				}
+				else
+				{
+					response.success();
+				}
+			},
+			error: function(error)
+			{
+				response.error("Could not validate uniqueness for this RemoteRecipient object.");
+			}
+		});
+	}
+});
